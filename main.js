9cbede5c7d9437b68b95a12d9f5d89f020aeef2d
Nicholas Auer <nick.nickauer@gmail.com>
2018-11-29 18:27:37 -0700

    wh

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.use(express.static(path));
 
 app.get('/', (req, res) => res.sendFile(path + 'home.html'))
 
-app.get('/', function (req, res) {
+app.get('/verify', function (req, res) {
     res.sendFile(path + "login.html")
 })
 
