f4c307bd182f8bf236d0af85cbb474bf116245da
Chandler de Spirlet <chandler.despirlet@icloud.com>
2018-11-29 17:20:00 -0700

    PSQL function

--- verify.js
+++ verify.js
@@ -5,7 +5,7 @@ app.put('/login/verify', function (req, res) {
     req.assert("user","user required").notEmpty();
     req.assert("pass","pass required").notEmpty();
     
-    console.log("test")
+    console.log("test");
     
     var errors = req.validationErrors();
     
@@ -14,15 +14,30 @@ app.put('/login/verify', function (req, res) {
             user: req.santitize("user").escape().trim(),
             pass: req.santitize("pass").escape().trim()
         };
+        /*
         db.none("SELECT * FROM login" +
                     "WHERE username = " + id.user +
                         "AND hashvalue = " + id.pass //TODO: do a hash here thx
+                        */
+        db.func('checkuser', [id.user, id.pass])
+            .then( data => {
+                var temp = data[0];
+                var final = temp.checkuser;
+                if (final == true){
+                    res.render("home.html");
+                }
+                else {
+                    res.render("login.html");
+                }
+            /*
         ).then(function (res) {
                     //res.flash("success","login")
                     res.render("about.html")
                 }
         )
+        */
         
-        res.sendFile("about.html")
+        //res.sendFile("about.html")
+        })
     }
 });
