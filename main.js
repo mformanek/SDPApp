5859e551b4d4158bf557fd3686a99581f4631f35
Nicholas Auer <nick.nickauer@gmail.com>
2018-11-29 18:30:04 -0700

    all-in-one superstore

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const db = require("./database.js")
-const vf = require("./verify.js")
+//const vf = require("./verify.js")
 const app = express()
 
 module.exports = app;
@@ -18,6 +18,36 @@ app.get('/verify', function (req, res) {
     res.sendFile(path + "login.html")
     console.log("Here I am in app.get(verify)");
 })
+
+app.post('/verify', function (req, res) {
+    console.log("Hello world");
+    req.assert("user","user required").notEmpty();
+    req.assert("pass","pass required").notEmpty();
+
+    console.log("test");
+    
+    var errors = req.validationErrors();
+    
+    if (!errors) {
+        var id = {
+            user: req.santitize("user").escape().trim(),
+            pass: req.santitize("pass").escape().trim()
+        };
+        db.func('checkuser', [id.user, id.pass])
+            .then( data => {
+                var temp = data[0];
+                var final = temp.checkuser;
+                console.log("temp =", temp);
+                console.log("final =", final);
+                if (final == true){
+                    res.redirect("home.html");
+                }
+                else {
+                    res.redirect("home.html");
+                }
+        })
+    }
+});
 
 app.use("/verify", vf)
 
