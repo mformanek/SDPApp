b7af5c1bf62e873bf8f4552981b29eb51372a7dd
Nicholas Auer <nick.nickauer@gmail.com>
2018-12-05 18:04:23 -0700

    alert test 2

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const sess = require("express-session");
 var exp_val = require('express-validator');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var popup = require("popups");
 
 const app = express();
 app.use(bodyParser.json());
@@ -66,7 +66,9 @@ app.post('/verify', function (req, res) {
         })
     }
     else {
-        alert("Erorr, did you put in a unsername and a password?");
+       popup.alert({
+           content:"Erorr, did you put in a unsername and a password?"
+           });
         res.redirect("login.html");
     }
     console.log("ding! the function's done");
