171762c9308e2be2934fbc6d1eb8ffc3f3de71b5
mformanek <mourrison@live.com>
2018-12-18 20:19:40 -0700

    clean up

--- New folder/New folder/testrunner.js
+++
@@ -1,6 +0,0 @@
-const TestRunner = require('test-runner')
-const runner = new TestRunner()
-
-runner.test('this will pass', function () {
-  const thought = 'Nothing failing here.'
-})
--- New folder/database.js
+++
@@ -1,5 +0,0 @@
-var pgp = require("pg-promise")();
-var dbConfig = process.env.DATABASE_URL;
-var db = pgp(dbConfig);
-
-module.exports = db;
